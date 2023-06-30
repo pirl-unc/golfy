7a181c0bf6ce9106ab8380c66dd94fbc1d573faa
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2023-06-30 17:23:43 -0400

    add wells when not coverging

--- golfy/__init__.py
+++ golfy/__init__.py
@@ -1,18 +1,110 @@
-from typing import Iterable
 from collections import defaultdict
+from dataclasses import dataclass
+from typing import Iterable, Optional, Mapping
+import random
 
 import numpy as np
 
 
-# type aliases
-Solution = dict[int, dict[int, Iterable[int]]]
-ConstraintList = Iterable[tuple[int, int]]
+Replicate = int
+Peptide = int
+Pool = int
+SwapCandidateList = Iterable[tuple[Replicate, Pool, Peptide]]
+ReplicateToNeighborDict = Mapping[Replicate, Mapping[Peptide, set[Peptide]]]
+PeptidePairList = Iterable[tuple[Peptide, Peptide]]
+
+
+@dataclass
+class Spec:
+    num_peptides: int
+    max_peptides_per_pool: int
+    num_replicates: int
+    invalid_neighbors: PeptidePairList
+    preferred_neighbors: PeptidePairList
+
+
+@dataclass
+class Solution(Spec):
+    assignments: Mapping[Replicate, Mapping[Pool, Iterable[Peptide]]]
+
+    def move_peptide(
+        self,
+        replicate_idx: Replicate,
+        old_pool_idx: Pool,
+        peptide: Peptide,
+        new_pool_idx: Pool,
+    ):
+        """
+        Move a peptide from its current pool to a new pool
+        """
+        pool_to_peptides = self.assignments[replicate_idx]
+        old_pool = pool_to_peptides[old_pool_idx]
+        new_pool = pool_to_peptides[new_pool_idx]
+        assert len(new_pool) < self.max_peptides_per_pool
+        pool_to_peptides[new_pool_idx] = np.array(list(new_pool) + [peptide])
+
+        pool_to_peptides[old_pool_idx] = np.array([p for p in old_pool if p != peptide])
+
+    def swap_peptides(
+        self,
+        replicate_idx: Replicate,
+        pool_idx_a: Pool,
+        peptide_a: Peptide,
+        pool_idx_b: Pool,
+        peptide_b: Peptide,
+    ):
+        """
+        Move peptide A from its current pool to the pool of peptide B (and vice versa)
+        """
+        pool_to_peptides = self.assignments[replicate_idx]
+        pool_a = pool_to_peptides[pool_idx_a]
+        pool_b = pool_to_peptides[pool_idx_b]
+        pool_to_peptides[pool_idx_a] = np.array(
+            [i for i in pool_a if i != peptide_a] + [peptide_b]
+        )
+        pool_to_peptides[pool_idx_b] = np.array(
+            [i for i in pool_b if i != peptide_b] + [peptide_a]
+        )
+
+    def remove_empty_pools(self):
+        """
+        Delete any empty pools and renumber the pools to be contiguous
+        """
+        for replicate_idx, pool_to_peptides in self.assignments.items():
+            to_delete = [
+                pool_idx
+                for (pool_idx, pool) in pool_to_peptides.items()
+                if len(pool) == 0
+            ]
+            if len(to_delete) > 0:
+                for pool_idx in to_delete:
+                    del pool_to_peptides[pool_idx]
+                index_mapping = {
+                    old_idx: new_idx
+                    for (new_idx, old_idx) in enumerate(sorted(pool_to_peptides.keys()))
+                }
+                self.assignments[replicate_idx] = {
+                    index_mapping[pool_idx]: pool
+                    for (pool_idx, pool) in pool_to_peptides.items()
+                }
 
 
 def random_init(
-    num_peptides: int = 100, peptides_per_pool: int = 5, num_replicates: int = 3
+    num_peptides: int = 100,
+    peptides_per_pool: int = 5,
+    num_replicates: int = 3,
+    num_pools: Optional[int] = None,
+    invalid_neighbors: PeptidePairList = [],
+    preferred_neighbors: PeptidePairList = [],
 ) -> Solution:
-    num_pools = int(np.ceil(num_peptides / peptides_per_pool))
+    if num_pools is None:
+        num_pools = int(np.ceil(num_peptides / peptides_per_pool))
+
+    preferred_neighbor_dict = defaultdict(set)
+    for p1, p2 in preferred_neighbors:
+        preferred_neighbor_dict[p1].add(p2)
+        preferred_neighbor_dict[p2].add(p1)
+
     replicate_to_pool_to_peptides = {}
     for i in range(num_replicates):
         peptide_array = np.arange(num_peptides)
@@ -24,21 +116,27 @@ def random_init(
             start_idx = peptides_per_pool * j
             end_idx = peptides_per_pool * (j + 1)
             pool_assignments[j] = peptide_array[start_idx:end_idx]
-    return replicate_to_pool_to_peptides
+
+    return Solution(
+        num_peptides=num_peptides,
+        max_peptides_per_pool=peptides_per_pool,
+        num_replicates=num_replicates,
+        invalid_neighbors=invalid_neighbors,
+        preferred_neighbors=preferred_neighbors,
+        assignments=replicate_to_pool_to_peptides,
+    )
 
 
-def is_valid(
-    replicate_to_pool_to_peptides: Solution, invalid_neighbors: ConstraintList = []
-) -> bool:
+def is_valid(s: Solution) -> bool:
     peptide_to_neighbors = defaultdict(set)
 
-    if invalid_neighbors:
+    if s.invalid_neighbors:
         # treat invalid pairs as if they've already been neighbors in a previous round
-        for p1, p2 in invalid_neighbors:
+        for p1, p2 in s.invalid_neighbors:
             peptide_to_neighbors[p1].add(p2)
             peptide_to_neighbors[p2].add(p1)
 
-    for replicate_idx, pool_to_peptides in replicate_to_pool_to_peptides.items():
+    for replicate_idx, pool_to_peptides in s.assignments.items():
         replicate_num = replicate_idx + 1
 
         # first check each peptide occurs once per replicate
@@ -64,12 +162,13 @@ def is_valid(
     return True
 
 
-def count_violations(
-    replicate_to_pool_to_peptides: Solution, invalid_neighbors: ConstraintList = []
-) -> int:
+def count_violations(s: Solution) -> int:
     """
     Return the number of invalid peptide pairs in the solution
     """
+    invalid_neighbors = s.invalid_neighbors
+    replicate_to_pool_to_peptides = s.assignments
+
     peptide_to_neighbors = defaultdict(set)
 
     if invalid_neighbors:
@@ -100,19 +199,12 @@ def count_violations(
     return violations
 
 
-import random
-from typing import Mapping
-
-Replicate = int
-Peptide = int
-Pool = int
-SwapCandidateList = Iterable[tuple[Replicate, Pool, Peptide]]
-ReplicateToNeighborDict = Mapping[Replicate, Mapping[Peptide, set[Peptide]]]
-
-
 def find_violating_peptides(
-    replicate_to_pool_to_peptides: Solution, invalid_neighbors: ConstraintList = []
+    s: Solution,
 ) -> tuple[SwapCandidateList, ReplicateToNeighborDict]:
+    replicate_to_pool_to_peptides = s.assignments
+    invalid_neighbors = s.invalid_neighbors
+
     peptide_to_neighbors = defaultdict(set)
 
     if invalid_neighbors:
@@ -139,14 +231,11 @@ def find_violating_peptides(
     return needs_swap, replicate_to_neighbor_dict
 
 
-def improve_solution(
-    replicate_to_pool_to_peptides: Solution,
-    invalid_neighbors: ConstraintList = [],
-    verbose: bool = False,
-):
-    needs_swap, replicate_to_neighbor_dict = find_violating_peptides(
-        replicate_to_pool_to_peptides, invalid_neighbors
-    )
+def improve_solution(s: Solution, verbose: bool = False):
+    replicate_to_pool_to_peptides = s.assignments
+    invalid_neighbors = s.invalid_neighbors
+
+    needs_swap, replicate_to_neighbor_dict = find_violating_peptides(s)
 
     random.shuffle(needs_swap)
 
@@ -157,115 +246,163 @@ def improve_solution(
             continue
 
         pool_to_peptides = replicate_to_pool_to_peptides[replicate_idx]
+        pool_a = pool_to_peptides[pool_idx_a]
 
-        other_peptides = []
-        peptide_to_pool_idx = {}
-        for pool_idx_i, pool_peptides_i in pool_to_peptides.items():
-            if pool_idx_i != pool_idx_a and pool_idx_i not in swapped_pools:
-                all_peptides_ok = True
-                for p in pool_peptides_i:
-                    peptide_to_pool_idx[p] = pool_idx_i
-                    all_peptides_ok = all_peptides_ok and (
-                        peptide_a not in replicate_to_neighbor_dict[replicate_idx][p]
+        # if a pool is empty, just move the offending peptide there
+        empty_pools = {
+            pool_idx
+            for (pool_idx, pool_peptides) in pool_to_peptides.items()
+            if len(pool_peptides) == 0
+        }
+        if empty_pools:
+            pool_idx_b = random.choice(list(empty_pools))
+            s.move_peptide(replicate_idx, pool_idx_a, peptide_a, pool_idx_b)
+            swapped_pools.add(pool_idx_a)
+            swapped_pools.add(pool_idx_b)
+            swapped_peptides.add(peptide_a)
+        else:
+            other_peptides = []
+            peptide_to_pool_idx = {}
+            for pool_idx_i, pool_peptides_i in pool_to_peptides.items():
+                if pool_idx_i != pool_idx_a and pool_idx_i not in swapped_pools:
+                    all_peptides_ok = True
+                    for p in pool_peptides_i:
+                        peptide_to_pool_idx[p] = pool_idx_i
+                        all_peptides_ok = all_peptides_ok and (
+                            peptide_a
+                            not in replicate_to_neighbor_dict[replicate_idx][p]
+                        )
+                    if all_peptides_ok:
+                        other_peptides.extend(
+                            [p for p in pool_peptides_i if p not in swapped_peptides]
+                        )
+
+            if len(other_peptides) == 0:
+                if verbose:
+                    print(
+                        "Not able to find a valid peptide to swap with for (%s, %s, %s)"
+                        % (replicate_idx, pool_idx_a, peptide_a)
                     )
-                if all_peptides_ok:
-                    other_peptides.extend(
-                        [p for p in pool_peptides_i if p not in swapped_peptides]
+                continue
+            peptide_b = random.choice(other_peptides)
+            pool_idx_b = peptide_to_pool_idx[peptide_b]
+            assert peptide_a != peptide_b
+            assert pool_idx_a != pool_idx_b
+            pool_b = pool_to_peptides[pool_idx_b]
+            if (
+                len(pool_a) > 1
+                and len(pool_b) < s.max_peptides_per_pool
+                and random.choice([False, True])
+            ):
+                # just move peptide a to the pool with fewer than max peptides
+                if verbose:
+                    print(
+                        "Moving peptide %d from pool %d to pool %d"
+                        % (peptide_a, pool_idx_a, pool_idx_b)
+                    )
+                s.move_peptide(replicate_idx, pool_idx_a, peptide_a, pool_idx_b)
+                swapped_pools.add(pool_idx_a)
+                swapped_pools.add(pool_idx_b)
+                swapped_peptides.add(peptide_a)
+            else:
+                if verbose:
+                    print("Before swap")
+                    print(
+                        "pool",
+                        pool_idx_a,
+                        "peptide",
+                        peptide_a,
+                        pool_to_peptides[pool_idx_a],
+                    )
+                    print(
+                        "pool",
+                        pool_idx_b,
+                        "peptide",
+                        peptide_b,
+                        pool_to_peptides[pool_idx_b],
                     )
 
-        if len(other_peptides) == 0:
-            if verbose:
-                print(
-                    "Not able to find a valid peptide to swap with for (%s, %s, %s)"
-                    % (replicate_idx, pool_idx_a, peptide_a)
+                # actually swap them
+                s.swap_peptides(
+                    replicate_idx, pool_idx_a, peptide_a, pool_idx_b, peptide_b
                 )
-            continue
-        peptide_b = random.choice(other_peptides)
-        pool_idx_b = peptide_to_pool_idx[peptide_b]
-        assert peptide_a != peptide_b
-        assert pool_idx_a != pool_idx_b
-        if verbose:
-            print("Before swap")
-            print(
-                "pool", pool_idx_a, "peptide", peptide_a, pool_to_peptides[pool_idx_a]
-            )
-            print(
-                "pool", pool_idx_b, "peptide", peptide_b, pool_to_peptides[pool_idx_b]
-            )
 
-        # actually swap them
-        pool_to_peptides[pool_idx_a] = np.array(
-            [i for i in pool_to_peptides[pool_idx_a] if i != peptide_a] + [peptide_b]
-        )
-        pool_to_peptides[pool_idx_b] = np.array(
-            [i for i in pool_to_peptides[pool_idx_b] if i != peptide_b] + [peptide_a]
-        )
+                if verbose:
+                    print("After")
+                    print(
+                        "pool",
+                        pool_idx_a,
+                        "peptide",
+                        peptide_a,
+                        pool_to_peptides[pool_idx_a],
+                    )
+                    print(
+                        "pool",
+                        pool_idx_b,
+                        "peptide",
+                        peptide_b,
+                        pool_to_peptides[pool_idx_b],
+                    )
 
-        if verbose:
-            print("After")
-            print(
-                "pool", pool_idx_a, "peptide", peptide_a, pool_to_peptides[pool_idx_a]
-            )
-            print(
-                "pool", pool_idx_b, "peptide", peptide_b, pool_to_peptides[pool_idx_b]
-            )
-
-        swapped_pools.add(pool_idx_a)
-        swapped_pools.add(pool_idx_b)
-        swapped_peptides.add(peptide_a)
-        swapped_peptides.add(peptide_b)
-
-    return replicate_to_pool_to_peptides
+                swapped_pools.add(pool_idx_a)
+                swapped_pools.add(pool_idx_b)
+                swapped_peptides.add(peptide_a)
+                swapped_peptides.add(peptide_b)
 
 
 def optimize(
-    replicate_to_pool_to_peptides: Solution,
-    invalid_neighbors: ConstraintList = [],
+    s: Solution,
     max_iters: int = 100,
     verbose: bool = False,
+    add_pool_if_stuck: bool = True,
     return_history: bool = False,
 ) -> bool:
     """
     Iteratively update solution by randomly swapping a violating peptide with a random other peptide
 
-    Parameters
-    ----------
-    replicate_to_pool_to_peptides:
+    Args
+    ----
+    solution
         Initial solution which will be modified in-place
 
-    invalid_neighbors:
-        Constraint list of peptide pairs which cannot be placed together
-
-    max_iters:
+    max_iters
         Maximum number of swaps to consider performing
 
-    verbose:
+    verbose
         Print number of violations for each iteration
 
-    return_history:
+    add_pool_if_stuck
+        If no improvements have been made for 10 iters, add a pool
+        to the last replicate
+
+    return_history
         Return array of constraint validation counts per iteration
 
 
-    Returns
-    -------
-    True if non-violating solution found, False if solution still has violations after
+    Returns True if non-violating solution found, False if solution still has violations after
     max_iters
     """
-    old_num_violations = count_violations(
-        replicate_to_pool_to_peptides, invalid_neighbors
-    )
+    replicate_to_pool_to_peptides = s.assignments
+    invalid_neighbors = s.invalid_neighbors
+
+    old_num_violations = count_violations(s)
     history = [old_num_violations]
     if verbose:
         print("Initial solution has %s violations" % (old_num_violations,))
+    num_iters_without_improvement = 0
     for i in range(max_iters):
         history.append(old_num_violations)
-        improve_solution(replicate_to_pool_to_peptides, invalid_neighbors)
-        new_num_violations = count_violations(
-            replicate_to_pool_to_peptides, invalid_neighbors
-        )
+        improve_solution(s)
+        new_num_violations = count_violations(s)
+
         history.append(new_num_violations)
         if verbose:
             print("%d) %d -> %d" % (i + 1, old_num_violations, new_num_violations))
+
+        if old_num_violations <= new_num_violations:
+            num_iters_without_improvement += 1
+        else:
+            num_iters_without_improvement = 0
 
         old_num_violations = new_num_violations
 
@@ -273,6 +410,20 @@ def optimize(
             if verbose:
                 print("Found valid solution after %d swaps" % (i + 1,))
             break
+
+        if num_iters_without_improvement > 10 and add_pool_if_stuck:
+            last_replicate_idx = s.num_replicates - 1
+            last_replicate = s.assignments[last_replicate_idx]
+            num_pools = len(last_replicate)
+            last_replicate[num_pools] = np.array([])
+            if verbose:
+                print(
+                    "Adding pool %d to replicate %d"
+                    % (num_pools, last_replicate_idx + 1)
+                )
+
+    # just in case we ended up with any empty pools, remove them from the solution
+    s.remove_empty_pools()
 
     result = old_num_violations == 0
     if return_history:
--- tests/test_init.py
+++ tests/test_init.py
@@ -3,6 +3,6 @@ from golfy import random_init
 
 def test_random_init():
     s = random_init(num_peptides=100, peptides_per_pool=5, num_replicates=3)
-    assert len(s) == 3
-    assert len(s[0]) == 20
-    assert len(s[0][0]) == 5
+    assert len(s.assignments) == 3
+    assert len(s.assignments[0]) == 20
+    assert len(s.assignments[0][0]) == 5
