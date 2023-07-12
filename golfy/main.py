6ccc5e64ebd673546c340d4bd99bc526403eaa99
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2023-07-12 16:43:03 -0400

    added docstring to find_best_solution

--- golfy/main.py
+++ golfy/main.py
@@ -19,7 +19,36 @@ def find_best_solution(
 ) -> Solution:
     """
     Try several different initialization methods and return the solution which
-    is optimized to have the fewest violations and the fewest pools
+    is optimized to have the fewest violations and the fewest pools.
+
+    Args
+    ----
+    num_peptides
+        The number total number of peptides in the experiment
+
+    max_peptides_per_pool
+        Maximum number of peptides which can be in any one pool. This is also the number of peptides
+        which will be in most pools if allow_extra_pools is False
+
+    num_replicates
+        Number of replicates in the experiment (i.e. how many distinct pools each peptide occurs in)
+
+    num_pools_per_replicate
+        Number of pools in each replicate. If None, then this will be set to the minimum number of pools
+        required to fit all peptides. If an int, then this will be the number of pools in each replicate.
+
+    invalid_neighbors
+        List of peptide pairs which cannot be in the same pool
+
+    preferred_neighbors
+        List of peptide pairs which should be in the same pool if possible
+
+    allow_extra_pools
+        If True, then the solution can have more than the minimum number of pools required to fit all peptides. If False,
+        then the returned solution may not be valid (i.e. some peptide pairs will occur in more than one pool together)
+
+    verbose
+        If True, then print out information about the solution as it is being constructed
     """
     shared_kwargs = dict(
         num_peptides=num_peptides,
