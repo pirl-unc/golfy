bd0109659ce46c300ebfedbc36affb7a63f4cfae
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2024-02-10 21:29:03 -0500

    added future import

--- golfy/__init__.py
+++ golfy/__init__.py
@@ -1,3 +1,17 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
 from .deconvolution import (
     create_linear_system,
     solve_linear_system,
@@ -17,7 +31,7 @@ from .simulation import (
 from .types import SpotCounts
 from .validity import is_valid, count_violations, violations_per_replicate
 
-__version__ = "2.5.0"
+__version__ = "2.5.1"
 
 __all__ = [
     "__version__",
